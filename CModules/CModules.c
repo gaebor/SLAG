5b814a97396270640e633a657dbc81f93f652078
gaebor <gaebor@gmail.com>
2018-03-10 23:07:01 +0100

    new cmodule

--- CModules/CModules.c
+++ CModules/CModules.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "slag/slag_interface.h"
@@ -41,6 +42,45 @@ void** Mul(void** input, int inputPortNumber, int* outputPortNumber)
 	return &result;
 }
 
+FILE* input_for_read;
+int* msg;
+
+void** Read(void** input, int inputPortNumber, int* outputPortNumber)
+{
+    static int i;
+    void** result = NULL;
+
+    *outputPortNumber = 0;
+    if (fscanf(input_for_read, "%d", &i) == 1)
+    {
+        msg = (int*)malloc(sizeof(int));
+        if (msg)
+        {
+            *msg = i;
+            *outputPortNumber = 1;
+            result = &msg;
+        }
+    }
+    return result;
+}
+
+SLAG_MODULE_EXPORT(int) SlagInitialize(
+    void* module,
+    int settingsc, const char** settingsv,
+    void* txtin, void* txtout,
+    const char** strout, int* strout_size,
+    unsigned char** out_img, int* w, int* h, enum ImageType imageType)
+{
+
+    if (module == Read)
+    {
+        if (settingsc > 0)
+            input_for_read = fopen(settingsv[0], "r");
+    }
+    return 0;
+}
+
+
 SLAG_MODULE_EXPORT(void*) SlagInstantiate(const char* moduleName, const char* InstanceName)
 {
 	SlagFunction_t function = NULL;
@@ -49,7 +89,8 @@ SLAG_MODULE_EXPORT(void*) SlagInstantiate(const char* moduleName, const char* In
 		function = Add;
 	else if (0 == strcmp("Mul", moduleName))
 		function = Mul;
-
+    else if (0 == strcmp("Read", moduleName))
+        function = Read;
 	return (void*)function;
 }
 
@@ -65,4 +106,8 @@ SLAG_MODULE_EXPORT(void) SlagDestroyMessage(void* message)
 
 SLAG_MODULE_EXPORT(void) SlagDestroyModule(void* module)
 {
+    if (module == Read)
+    {
+        fclose(input_for_read);
+    }
 }
